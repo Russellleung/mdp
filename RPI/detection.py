c2e40994727711327e2f2d184713907f67aac046
hungrhea <yuntingang140201@gmail.com>
2022-09-26 11:45:39 +0800

    updated detection.py and server.py

--- RPI/detection.py
+++ RPI/detection.py
@@ -11,8 +11,7 @@ import json
 
 print(f"Setup complete. Using torch {torch.__version__} ({torch.cuda.get_device_properties(0).name if torch.cuda.is_available() else 'CPU'})")
 
-p = subprocess.getstatusoutput("python detect.py --weights best_v3.pt --save-conf --img 640 --conf 0.1 --source ./imagezmq_images") 
-print(p[1])
+p = subprocess.getstatusoutput("python detect.py --weights best_v3.pt --img 640 --conf 0.6 --source ./imagezmq_images") 
 output = p[1]
 
 with open('outputs/output_test.txt', 'w') as f:    # path to output .txt file
@@ -37,18 +36,23 @@ def process_output(path = None, string = None):
   #a list of only strings of image lines in output
   raw = [line for line in raw if (line[:5]=="image")]
   output = {}
-  detected_classes = []
   for line in raw:
+    # determine image file type
+    if ".jpg" in line:
+      name_tail = ".jpg:"
+    elif ".jpeg" in line:
+      name_tail = ".jpeg:"
+    elif ".png" in line:
+      name_tail = ".png:"
+      
     #split the line into words
     line_split = line.split()
-    name = [i for i in line_split if i.endswith(".jpg:")][0]
-    print(name)
+    name = [i for i in line_split if i.endswith(name_tail)][0]
     name = name[:-1] #remove : from image name
     # no classes detected
     #last word
     if line_split[-1].endswith("ms"):
       detections = None
-      confidences = None
     # classes detected
     else:
       for e in line_split:
@@ -70,20 +74,49 @@ def process_output(path = None, string = None):
       assert(len(classes_with_duplicates) == len(confidences))
       for i in range(len(confidences)):
         #added
-        detected_classes.append(classes_with_duplicates[i])
         detections.append([classes_with_duplicates[i], float(confidences[i])])
     output[name] = detections
-    print(detections)
-    print(confidences)
   # return output
-  if detections:
-    print(detections[0][0])
-    return detections[0][0] 
+  if len(output) != 0:
+    return output
   else:
-    return 1
+    return None
 
   # return detected_classes
 
 # print(process_output(path = "outputs/output_test.txt"))
 # print(json.dumps(process_output(path = "outputs/output_test.txt"), indent=4))    # path to output .txt file
+
+
+
+
+
+
+# returns tuple (image_name, detected_class)
+def highest_conf(message_dict):
+  if message_dict:
+    keys = message_dict.keys()
+    # initialise image_name, detected_class and max_conf
+    detected_class = None
+    for key in keys:
+      if message_dict[key]:
+        max_conf = message_dict[key][0][1]
+        image_name = key
+        detected_class = message_dict[key][0][0]
+    # no images contain a detection
+    if detected_class == None:
+      print("No detections found in images")
+      return None
+
+    for key in keys:
+      for detection in message_dict[key]:
+        if detection[1] > max_conf:
+          max_conf = detection[1]
+          image_name = key
+          detected_class = detection[0]
+    
+    return (image_name, detected_class)
+  print("No images in detection")
+  return None
+
 
