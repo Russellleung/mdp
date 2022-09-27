2f09f3a62f50cb838a702fc691739749cef572b2
JLZJ <65708879+JLZJ@users.noreply.github.com>
2022-09-27 18:53:47 +0800

    Basic mapping
    2 TODOs

--- RPI_and_Algorithm_final-code/carpath.py
+++ RPI_and_Algorithm_final-code/carpath.py
@@ -9,10 +9,11 @@
 import sys
 import numpy as np
 import math
+import time
 from map import *
 
-YDIS = 15
-XDIS = 35
+YDIS = 7
+XDIS = 3
 
 class Node:
     def __init__(self, row, col, direction):
@@ -22,12 +23,12 @@ class Node:
         self.col = col
         self.direction = direction
 
-        self.h = 0
-        self.g = 0
         self.f = 0
+        self.g = 0
+        self.h = 0
 
     def updatef(self):
-        self.f = self.g + self.h
+        self.f = self.g + 1.2*self.h # TODO: attempt heuristic value set to 1.2 - need to determine whether optimal path provided
 
     def getcoord(self):
         return (self.row, self.col)
@@ -39,7 +40,8 @@ def manhattanDistance(cur, end):
     return abs(cur.row - end.row) + abs(cur.col - end.col)
 
 def carwithinArea(row, col):
-    return row > -1 + 15 and row < 201 - 15 and col > -1 + 15 and col < 201 - 15
+    return row > 2 and row < 38 and col > 2 and col < 38
+    # return row > -1 + 15 and row < 201 - 15 and col > -1 + 15 and col < 201 - 15
 
 def getnextLoc(node):
     nextloc = []
@@ -49,8 +51,8 @@ def getnextLoc(node):
     checkleftfwd(node, nextloc)
     checkrightrev(node, nextloc)
     checkleftrev(node, nextloc)
-    if (node.row % 5 != 0 or node.col % 5 != 0):
-        adjust(node, nextloc)
+    # if (node.row % 5 != 0 or node.col % 5 != 0):
+    #     adjust(node, nextloc)
     return nextloc
 
 def adjust(node, nextloc):
@@ -67,92 +69,135 @@ def checkfwd(node, nextloc):
     if node.prevaction == 's':
         return
     elif node.direction == 'n':
-        nextloc.append((node.row - 5, node.col, 'n', 'w005'))
-        nextloc.append((node.row - 10, node.col, 'n', 'w010'))
-        nextloc.append((node.row - 15, node.col, 'n', 'w015'))
-        nextloc.append((node.row - 20, node.col, 'n', 'w020'))
+        nextloc.append((node.row + 1, node.col, 'n', 'w005'))
+        nextloc.append((node.row + 2, node.col, 'n', 'w010'))
+        nextloc.append((node.row + 3, node.col, 'n', 'w015'))
+        nextloc.append((node.row + 4, node.col, 'n', 'w020'))
+        nextloc.append((node.row + 5, node.col, 'n', 'w025'))
+        nextloc.append((node.row + 6, node.col, 'n', 'w030'))
+        nextloc.append((node.row + 7, node.col, 'n', 'w035'))
+        nextloc.append((node.row + 8, node.col, 'n', 'w040'))
     elif node.direction == 'e':
-        nextloc.append((node.row, node.col + 5, 'e', 'w005'))
-        nextloc.append((node.row, node.col + 10, 'e', 'w010'))
-        nextloc.append((node.row, node.col + 15, 'e', 'w015'))
-        nextloc.append((node.row, node.col + 20, 'e', 'w020'))
+        nextloc.append((node.row, node.col + 1, 'e', 'w005'))
+        nextloc.append((node.row, node.col + 2, 'e', 'w010'))
+        nextloc.append((node.row, node.col + 3, 'e', 'w015'))
+        nextloc.append((node.row, node.col + 4, 'e', 'w020'))
+        nextloc.append((node.row, node.col + 5, 'e', 'w025'))
+        nextloc.append((node.row, node.col + 6, 'e', 'w030'))
+        nextloc.append((node.row, node.col + 7, 'e', 'w035'))
+        nextloc.append((node.row, node.col + 8, 'e', 'w040'))
     elif node.direction == 's':
-        nextloc.append((node.row + 5, node.col, 's', 'w005'))
-        nextloc.append((node.row + 10, node.col, 's', 'w010'))
-        nextloc.append((node.row + 15, node.col, 's', 'w015'))
-        nextloc.append((node.row + 20, node.col, 's', 'w020')) 
+        nextloc.append((node.row - 1, node.col, 's', 'w005'))
+        nextloc.append((node.row - 2, node.col, 's', 'w010'))
+        nextloc.append((node.row - 3, node.col, 's', 'w015'))
+        nextloc.append((node.row - 4, node.col, 's', 'w020')) 
+        nextloc.append((node.row - 5, node.col, 's', 'w025'))
+        nextloc.append((node.row - 6, node.col, 's', 'w030'))
+        nextloc.append((node.row - 7, node.col, 's', 'w035'))
+        nextloc.append((node.row - 8, node.col, 's', 'w040')) 
     elif node.direction == 'w':
-        nextloc.append((node.row, node.col - 5, 'w', 'w005'))
-        nextloc.append((node.row, node.col - 10, 'w', 'w010'))
-        nextloc.append((node.row, node.col - 15, 'w', 'w015'))
-        nextloc.append((node.row, node.col - 20, 'w', 'w020'))
+        nextloc.append((node.row, node.col - 1, 'w', 'w005'))
+        nextloc.append((node.row, node.col - 2, 'w', 'w010'))
+        nextloc.append((node.row, node.col - 3, 'w', 'w015'))
+        nextloc.append((node.row, node.col - 4, 'w', 'w020'))
+        nextloc.append((node.row, node.col - 5, 'w', 'w025'))
+        nextloc.append((node.row, node.col - 6, 'w', 'w030'))
+        nextloc.append((node.row, node.col - 7, 'w', 'w035'))
+        nextloc.append((node.row, node.col - 8, 'w', 'w040'))
 
 def checkrev(node, nextloc):
     if node.prevaction == 'w':
         return
     elif node.direction == 'n':
-        nextloc.append((node.row + 5, node.col, 'n', 's'))
+        nextloc.append((node.row - 1, node.col, 'n', 's'))
+        nextloc.append((node.row - 2, node.col, 'n', 's'))
+        nextloc.append((node.row - 3, node.col, 'n', 's'))
+        nextloc.append((node.row - 4, node.col, 'n', 's'))
+        nextloc.append((node.row - 5, node.col, 'n', 's'))
+        nextloc.append((node.row - 6, node.col, 'n', 's'))
+        nextloc.append((node.row - 7, node.col, 'n', 's'))
+        nextloc.append((node.row - 8, node.col, 'n', 's'))
     elif node.direction == 'e':
+        nextloc.append((node.row, node.col - 1, 'e', 's'))
+        nextloc.append((node.row, node.col - 2, 'e', 's'))
+        nextloc.append((node.row, node.col - 3, 'e', 's'))
+        nextloc.append((node.row, node.col - 4, 'e', 's'))
         nextloc.append((node.row, node.col - 5, 'e', 's'))
+        nextloc.append((node.row, node.col - 6, 'e', 's'))
+        nextloc.append((node.row, node.col - 7, 'e', 's'))
+        nextloc.append((node.row, node.col - 8, 'e', 's'))
     elif node.direction == 's':
-        nextloc.append((node.row - 5, node.col, 's', 's'))
+        nextloc.append((node.row + 1, node.col, 's', 's'))
+        nextloc.append((node.row + 2, node.col, 's', 's'))
+        nextloc.append((node.row + 3, node.col, 's', 's'))
+        nextloc.append((node.row + 4, node.col, 's', 's'))
+        nextloc.append((node.row + 5, node.col, 's', 's'))
+        nextloc.append((node.row + 6, node.col, 's', 's'))
+        nextloc.append((node.row + 7, node.col, 's', 's'))
+        nextloc.append((node.row + 8, node.col, 's', 's'))
     elif node.direction == 'w':
+        nextloc.append((node.row, node.col + 1, 'w', 's'))
+        nextloc.append((node.row, node.col + 2, 'w', 's'))
+        nextloc.append((node.row, node.col + 3, 'w', 's'))
+        nextloc.append((node.row, node.col + 4, 'w', 's'))
         nextloc.append((node.row, node.col + 5, 'w', 's'))
+        nextloc.append((node.row, node.col + 6, 'w', 's'))
+        nextloc.append((node.row, node.col + 7, 'w', 's'))
+        nextloc.append((node.row, node.col + 8, 'w', 's'))
 
-def checkrightfwd(node, nextloc):
-    if node.prevaction == 'd':
-        return
-    elif node.direction == 'n':
-        nextloc.append((node.row - YDIS, node.col + XDIS, 'e', 'e'))
-    elif node.direction == 'e':
-        nextloc.append((node.row + XDIS, node.col + YDIS, 's', 'e'))
-    elif node.direction == 's':
-        nextloc.append((node.row + YDIS, node.col - XDIS, 'w', 'e'))
-    elif node.direction == 'w':
-        nextloc.append((node.row - XDIS, node.col - YDIS, 'n', 'e'))
-
-def checkleftfwd(node, nextloc):
-    if node.prevaction == 'a':
-        return
-    elif node.direction == 'n':
-        nextloc.append((node.row - YDIS, node.col - XDIS, 'w', 'q'))
-    elif node.direction == 'e':
-        nextloc.append((node.row - XDIS, node.col + YDIS, 'n', 'q'))
-    elif node.direction == 's':
-        nextloc.append((node.row + YDIS, node.col + XDIS, 'e', 'q'))
-    elif node.direction == 'w':
-        nextloc.append((node.row + XDIS, node.col - YDIS, 's', 'q'))
-
-def checkrightrev(node, nextloc):
-    if node.prevaction == 'e':
-        return
-    elif node.direction == 'n':
-        nextloc.append((node.row + XDIS, node.col + YDIS, 'w', 'd'))
-    elif node.direction == 'e':
-        nextloc.append((node.row + YDIS, node.col - XDIS, 'n', 'd'))
-    elif node.direction == 's':
-        nextloc.append((node.row - XDIS, node.col - YDIS, 'e', 'd'))
-    elif node.direction == 'w':
-        nextloc.append((node.row - YDIS, node.col + XDIS, 's', 'd'))
-
-def checkleftrev(node, nextloc):
+def checkleftrev(node, nextloc): # a
     if node.prevaction == 'q':
         return
     elif node.direction == 'n':
-        nextloc.append((node.row + XDIS, node.col - YDIS, 'e', 'a'))
+        nextloc.append((node.row - YDIS, node.col - XDIS, 'e', 'a'))
     elif node.direction == 'e':
-        nextloc.append((node.row - YDIS, node.col - XDIS, 's', 'a'))
+        nextloc.append((node.row + XDIS, node.col - YDIS, 's', 'a'))
     elif node.direction == 's':
-        nextloc.append((node.row - XDIS, node.col + YDIS, 'w', 'a'))
+        nextloc.append((node.row + YDIS, node.col + XDIS, 'w', 'a'))
     elif node.direction == 'w':
-        nextloc.append((node.row + YDIS, node.col + XDIS, 'n', 'a'))
+        nextloc.append((node.row - XDIS, node.col + YDIS, 'n', 'a'))
 
-def checkTurningCollision(grid,currentNode, endNode):
-    
+def checkrightrev(node, nextloc): # d
+    if node.prevaction == 'e':
+        return
+    elif node.direction == 'n':
+        nextloc.append((node.row - YDIS, node.col + XDIS, 'w', 'd'))
+    elif node.direction == 'e':
+        nextloc.append((node.row - XDIS, node.col - YDIS, 'n', 'd'))
+    elif node.direction == 's':
+        nextloc.append((node.row + YDIS, node.col - XDIS, 'e', 'd'))
+    elif node.direction == 'w':
+        nextloc.append((node.row + XDIS, node.col + YDIS, 's', 'd'))
+
+def checkleftfwd(node, nextloc): # q
+    if node.prevaction == 'a':
+        return
+    elif node.direction == 'n':
+        nextloc.append((node.row + YDIS, node.col - XDIS, 'w', 'q'))
+    elif node.direction == 'e':
+        nextloc.append((node.row + XDIS, node.col + YDIS, 'n', 'q'))
+    elif node.direction == 's':
+        nextloc.append((node.row - YDIS, node.col + XDIS, 'e', 'q'))
+    elif node.direction == 'w':
+        nextloc.append((node.row - XDIS, node.col - YDIS, 's', 'q'))
+
+def checkrightfwd(node, nextloc): # e
+    if node.prevaction == 'd':
+        return
+    elif node.direction == 'n':
+        nextloc.append((node.row + YDIS, node.col + XDIS, 'e', 'e'))
+    elif node.direction == 'e':
+        nextloc.append((node.row - XDIS, node.col + YDIS, 's', 'e'))
+    elif node.direction == 's':
+        nextloc.append((node.row - YDIS, node.col - XDIS, 'w', 'e'))
+    elif node.direction == 'w':
+        nextloc.append((node.row + XDIS, node.col - YDIS, 'n', 'e'))
+
+def checkTurningCollision(map, currentNode, endNode):
     if (endNode[3] == 'w' or endNode[3] == 's' or endNode[3] == 'w001'): # if going forward/reverse
         return False
 
-    if grid[endNode[0]][endNode[1]] == 5:
+    if map.grid[endNode[0]][endNode[1]] == 5:
         return True
     
     return False
@@ -190,13 +235,13 @@ def parseCoord(obstacleString):
     return (row,col,direction)
 
 # find path to visit 
-def nearestNeightbour(n,obstacles):
+def nearestNeightbour(n, obstacles):
     path = []
     visited = [False for i in range(n)]
     visited[0] = True
     currentNodeIndex = 0
     path.append(0)
-    cost = calculateCost(n,0,obstacles,visited)
+    cost = calculateCost(n, 0, obstacles, visited)
     #print(cost)
     for i in range (n-1):
         closestDist = 9999
@@ -211,26 +256,26 @@ def nearestNeightbour(n,obstacles):
         path.append(closestDistIndex)
         visited[closestDistIndex] = True
         currentNodeIndex = closestDistIndex
-        cost = calculateCost(n,currentNodeIndex,obstacles,visited)
-        #print(cost)
+        cost = calculateCost(n, currentNodeIndex, obstacles, visited)
 
+    print(path)
     return path
 
-def calculateCost(n,index,obstacles,visited):
-
+def calculateCost(n, index, obstacles, visited):
     cost = [0 for i in range(n)]
     for i in range (n):
         if visited[i] == True or i == index:
             continue
-        cost[i] = travelTime(obstacles[index],obstacles[i])
+        cost[i] = travelTime(obstacles[index], obstacles[i])
     return cost
 
-def travelTime(currentNode,nextNode):
+def travelTime(currentNode, nextNode):
     time = (currentNode[0] - nextNode[0])**2 + (currentNode[1] - nextNode[1])**2
     return int(math.ceil(math.sqrt(time)))
 
 def bulldoze(map, start, end):
-    startNode = Node(start[0], start[1], start[2])
+    map = 0
+    startNode = Node(start[0], start[1], start[2]) # row, col, direction
     endNode = Node(end[0], end[1], end[2])
 
     unexploredList = []
@@ -260,11 +305,10 @@ def bulldoze(map, start, end):
         moves = getnextLoc(currentNode)
 
         for move in moves:
-
             nextrow = move[0]
             nextcol = move[1]
             
-            if not carwithinArea(nextrow, nextcol):
+            if not carwithinArea(nextrow//5, nextcol//5):
                 continue
 
             newNode = Node(nextrow, nextcol, move[2])
@@ -292,7 +336,6 @@ def bulldoze(map, start, end):
             unexploredList.append(next)
 
 def findpath(map,start,end):
-
     startNode = Node(start[0], start[1], start[2])
     endNode = Node(end[0], end[1], end[2])
 
@@ -302,6 +345,7 @@ def findpath(map,start,end):
     unexploredList.append(startNode)
 
     while unexploredList:
+        # print(unexploredList[-1].prevaction, 'XY')
         unexploredList.sort(key=lambda x: x.f)
         currentNode = unexploredList.pop(0)
         exploredList.append(currentNode)
@@ -323,17 +367,16 @@ def findpath(map,start,end):
         moves = getnextLoc(currentNode)
 
         for move in moves:
-
             nextrow = move[0]
             nextcol = move[1]
             
             if not carwithinArea(nextrow, nextcol):
                 continue
 
-            if checkTurningCollision(map,currentNode.getcoord(), move): #endNode.getcoord() = obstacles list #endNode.getcoord(), currentNode.getcoord(),
+            if checkTurningCollision(map, currentNode.getcoord(), move): 
                 continue
             
-            if map[nextrow][nextcol] >= 6:
+            if map.grid[nextrow][nextcol] >= 5:
                 continue
 
             newNode = Node(nextrow, nextcol, move[2])
@@ -359,13 +402,14 @@ def findpath(map,start,end):
                 continue
 
             unexploredList.append(next)
-    print("constrained path not found - bulldozing")
-    return bulldoze(map,start,end)
+    print(currentNode.prevaction)
+    print("Safe path not found - attempt bulldozing algorithm")
+    return bulldoze(map, start, end)
 
 
-#path is coordinates
-#[path[::-1], actions[::-1], direction[::-1]]
-def convertToRobot(pathList):
+# path is coordinates
+# [path[::-1], actions[::-1], direction[::-1]]
+def convertToRobot(pathList): # TODO: what are our admissible UART instructions
     instruction = []
     for i in range (len(pathList)):
         if pathList[i][:1] == 'w':
@@ -383,8 +427,8 @@ def convertToRobot(pathList):
     return instruction
 
 # send to android : robot, x, y, direction
-#path is coordinates
-#[path[::-1], actions[::-1], direction[::-1]]
+# path is coordinates
+# [path[::-1], actions[::-1], direction[::-1]]
 def convertToAndroid(pathList):
     instruction = []
     for i in range (len(pathList[0])):
@@ -394,32 +438,63 @@ def convertToAndroid(pathList):
     return instruction
 
 def instToFollow(nodePath, map, destination):
-    android = []
     allPaths = []
     combinedPaths = []
+    pathLocations = []
+
     for i in range(len(nodePath)-1):
-        path = findpath(map.grid, destination[nodePath[i]], destination[nodePath[i+1]])
+        print('Moving from: ', destination[nodePath[i]], ' -> ', destination[nodePath[i+1]])
+        path = findpath(map, destination[nodePath[i]], destination[nodePath[i+1]])
+        print(path)
         allPaths.append(convertToRobot(path[1]))
-        android.append(convertToAndroid(path))
+        pathLocation = []
+        for i in range(len(path[0])):
+            pathLocation += [list(path[0][i]) + [path[2][i]]]
+        pathLocations += [pathLocation]
     combinedPaths.append(allPaths)
-    combinedPaths.append(android)
-    print('combined paths')
+    
+    return combinedPaths, pathLocations
 
-    print(combinedPaths)
-    return combinedPaths  
-
-def pathFinder(pathFromAndroid):
-    map = Map()
-    print(map.grid)
-    obstacleList = convert(pathFromAndroid)
-    print(obstacleList)
+def pathFinder(pathFromAndroid, map):
+    obstacleList = pathFromAndroid
     destination = map.parseTargets(obstacleList)
-    print('test 1')
-    destination.insert(0,(185,15,'n'))
-    print(destination)
-    nodePath = nearestNeightbour(len(destination),destination)
-    print(nodePath)
-    appendedPath = instToFollow(nodePath, map, destination)
-    appendedPath.append(nodePath)
-    return appendedPath
 
+    destination.insert(0,(3,3,'n')) # start, n is facing down
+
+    destination = [i for i in destination if i != "No goal"]
+    print(destination)
+
+    nodePath = nearestNeightbour(len(destination), destination)
+    appendedPath, appendedLocations = instToFollow(nodePath, map, destination)
+    appendedPath.append(nodePath)
+    return appendedPath, appendedLocations
+
+def main():
+    map = Map()
+    obstacleList = [[60,60,'n'],[170,100,'e'],[180,40,'s'],[60,145,'n'],[25,120,'s']]
+    print(map.parseTargets(obstacleList))
+    map.show_grid(False)
+
+    combinedPath, locations = pathFinder(obstacleList, map)
+
+    print(locations)
+    # locate position and direction of robot 
+    start = [3,3,'n']
+    locations[0] = [start]+locations[0]
+
+    start_time = time.time()
+
+    for path in locations:
+        for location in path:
+            map.set_robot_pos(location)
+            map.show_grid()
+            print(location)
+            time.sleep(1.3)
+        time.sleep(2.5)
+
+    print("Elapsed time:",time.time()-start_time)
+    print('Simulation Complete!')
+
+
+if __name__ == "__main__":
+    main()
--- RPI_and_Algorithm_final-code/map.py
+++ RPI_and_Algorithm_final-code/map.py
@@ -1,67 +1,175 @@
 import numpy as np
+import os
+import copy
 
 class Map:
     def __init__(self):
-        self.grid = np.zeros((201,201))
+        self.grid = [[0 for i in range(41)] for i in range(41)]
+        self.grid_snapshot = copy.deepcopy(self.grid)
+        self.changed = False
+        self.robot = [3,3,'n']
 
-    def inMap(self, npy, npx):
+    def show_grid(self, updated = True):
+        key_map = {"0": ".", # empty
+               "1": "+", 
+               "2": "+", 
+               "3": "+",
+               "4": "+",
+               "5": ".",
+               "6": "#",
+               "7": "^",
+               "8": ">",
+               "9": "v",
+               "10": "<",
+               "11": "—",
+               "12": "|"}
+        format = (lambda x: key_map[str(x)])
+
+        if updated:
+            # os.system('clear') # for Mac/Linux
+            os.system('cls') # for Windows
+        else:
+            self.grid_snapshot = copy.deepcopy(self.grid)
+
+        for x in range(41):
+            for y in range(41):
+                if self.grid[x][y] == 1:
+                    self.grid_snapshot[x][y+6] = 12
+                    self.grid_snapshot[x-1][y+6] = 12
+                    self.grid_snapshot[x+1][y+6] = 12
+
+                elif self.grid[x][y] == 2:
+                    self.grid_snapshot[x+6][y] = 11
+                    self.grid_snapshot[x+6][y-1] = 11
+                    self.grid_snapshot[x+6][y+1] = 11
+
+                elif self.grid[x][y] == 3:
+                    self.grid_snapshot[x][y-6] = 12
+                    self.grid_snapshot[x-1][y-6] = 12
+                    self.grid_snapshot[x+1][y-6] = 12
+
+                elif self.grid[x][y] == 4:
+                    self.grid_snapshot[x-6][y] = 11
+                    self.grid_snapshot[x-6][y-1] = 11
+                    self.grid_snapshot[x-6][y+1] = 11
+
+        # self.grid_snapshot[]
+        for s in self.grid_snapshot[::-1]:
+            print(*[format(i) for i in s])
+
+    def set_robot_pos(self, newPos):
+        self.grid_snapshot = copy.deepcopy(self.grid)
+        self.robot = newPos
+        if newPos[2] == 'n':
+            for i in range(7):
+                for j in range(7):
+                    self.grid_snapshot[newPos[0]+i-3][newPos[1]+j-3] = 7
+        elif newPos[2] == 'e':
+            for i in range(7):
+                for j in range(7):
+                    self.grid_snapshot[newPos[0]+i-3][newPos[1]+j-3] = 8
+        elif newPos[2] == 's':
+            for i in range(7):
+                for j in range(7):
+                    self.grid_snapshot[newPos[0]+i-3][newPos[1]+j-3] = 9
+        elif newPos[2] == 'w':
+            for i in range(7):
+                for j in range(7):
+                    self.grid_snapshot[newPos[0]+i-3][newPos[1]+j-3] = 10
+
+    def scale_down(self, npy, npx, round=True):
+        if round:
+            scaled_npy = npy//5
+            scaled_npx = npx//5
+        else:
+            scaled_npy = npy/5
+            scaled_npx = npx/5
+        return scaled_npy, scaled_npx
+
+    def inMap(self, npy, npx, unscaled=True):
         #check if map coordinate is within range
-        if npy < 0 or npy > 200 or npx < 0 or npx > 200:
+        if unscaled:
+            npy, npx = self.scale_down(npy, npx)
+        if npy < 3 or npy > 38 or npx < 3 or npx > 38:
             return False
         else:
             return True
 
     def parseTargets(self, convtargets):
-        #gets targets from convert function, assuming (row, col, direction)
+        # gets targets from convert function, assuming (row, col, direction)
+        if self.changed == False:
+            self.changed = True
+        else:
+            self.grid = [[0 for i in range(41)] for i in range(41)]
+            self.changed = True
+
         goals = []
 
         for target in convtargets:
             row = target[0]
             col = target[1]
+            row, col = self.scale_down(row, col)
+            # print(row)
 
-            #update sim grid with targets
+            # update sim grid with targets
             self.grid[row][col] = 6
 
-            #create restricted zone around target
-            for i in range(51):
-                for j in range(51):
-                    npy = row-25 + i
-                    npx = col-25 + j
-                    if self.inMap(npy, npx):
+            # create restricted zone around target
+            for i in range(11):
+                for j in range(11):
+                    npy = row-5 + i
+                    npx = col-5 + j
+                    if self.inMap(npy, npx, unscaled = False):
                         self.grid[npy][npx] = max(5, self.grid[npy][npx])
 
-            #create movement-restricted zone around target
-            for i in range(41):
-                for j in range(41):
-                    npy = row-20 + i
-                    npx = col-20 + j
-                    if self.inMap(npy, npx):
+            # create movement-restricted zone around target
+            for i in range(3):
+                for j in range(3):
+                    npy = row-1 + i
+                    npx = col-1 + j
+                    if self.inMap(npy, npx, unscaled = False):
                         self.grid[npy][npx] = max(6, self.grid[npy][npx])
 
-            #returns goalstates that needs to be achieved
+            # returns goalstates that needs to be achieved
             tgtgoal = self.getGoal(target)
             goals.append(tgtgoal)
         
         return goals
 
     def getGoal(self, target):
-        #get where we want the vehicle to position for each target
-        #assumes goalstates will not fall into restricted area
+        # get where we want the vehicle to position for each target
+        # assumes goals are not in restricted area
         row = target[0]
         col = target[1]
         direction = target[2]
+        row, col = self.scale_down(row, col)
 
         if direction == 'n':
-            self.grid[row-35][col] = 4
-            return (row-35, col, 's')
+            if (row+7)>38:
+                return ('No goal')
+            self.grid[row+7][col] = 4
+            return (row+7, col, 's')
         elif direction == 'e':
-            self.grid[row][col+35] = 3
-            return (row, col+35, 'w')
+            if (col+7)>38:
+                return ('No goal')
+            self.grid[row][col+7] = 3
+            return (row, col+7, 'w')
         elif direction == 's':
-            self.grid[row+35][col] = 2
-            return (row+35, col, 'n')
+            if (row-7)<3:
+                return ('No goal')
+            self.grid[row-7][col] = 2
+            return (row-7, col, 'n')
         elif direction == 'w':
-            self.grid[row][col-35] = 1
-            return (row, col-35, "e")
-        
+            if (col-7)<3:
+                return ('No goal')
+            self.grid[row][col-7] = 1
+            return (row, col-7, "e")
+
+# 6 refers to target
+# 5 refers to collision zone
+# 4 refers to position to scan target and direction north
+# 3 refers to position to scan target and direction east
+# 2 refers to position to scan target and direction south
+# 1 refers to position to scan target and direction west
+
 
